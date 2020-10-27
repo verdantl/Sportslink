54a27badd93316fa5c628d2ac27a26568ba2ad73
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-10-27 00:19:56 -0400

    fixed styling of the searchbox

--- sportslink/src/components/AthleteDashboard.js
+++ sportslink/src/components/AthleteDashboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import TopBar from './TopBar'
 import SearchBox from './SearchBox'
+// import AthleteDashBoardProfile from './AthleteDashboardProfile'
 
 class AthleteDashboard extends React.Component{
     render(){
         return <div>
             <TopBar/>
+            {/* <AthleteDashBoardProfile/> */}
             <SearchBox/>
             </div>
     }
--- sportslink/src/components/SearchBox.js
+++ sportslink/src/components/SearchBox.js
@@ -1,24 +1,50 @@
 import React from 'react'
 import Checkbox from "@material-ui/core/Checkbox"
+import Select from "@material-ui/core/Select"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
+import FormControl from "@material-ui/core/FormControl"
+import InputLabel from "@material-ui/core/InputLabel"
 import "../components-css/SearchBox.css"
+
 
 class SearchBox extends React.Component{
     render(){
-        return <div>
+        return <div className="searchBox">
         <div className="searchTitle">
             <p className="garamond"> Search</p>
         </div>
-        <FormControlLabel
+        <FormControlLabel className="checkUser"
         control={<Checkbox  name="checkedAthlete" />}
         label="Athletes"
         />
 
-        <FormControlLabel
+        <FormControlLabel className="checkUser"
         control={<Checkbox name="checkedRecruiters" />}
         label="Recruiters"
         />
+        
+        <FormControl>
+            <InputLabel>
+            Filter preferences
+            </InputLabel>
+            <Select
+                native
+                value="Bobby"
+                inputProps={{
+                name: 'age',
+                id: 'age-native-simple',
+                }}
+                >
+                <option aria-label="None" value="" />
+                <option value={10}>Ten</option>
+                <option value={20}>Twenty</option>
+                <option value={30}>Filter preferences </option>
+            </Select>
+        </FormControl>
+
         </div>
+
+        
     }
 }
 
