abc94e9d4f2ca5413586c7b57a29bee5914cb664
mojoanna <joanna.mo@mail.utoronto.ca>
2020-10-30 15:18:03 -0400

    Added BrowerRouter and paths

--- sportslink/src/App.js
+++ sportslink/src/App.js
@@ -2,14 +2,24 @@ import React from 'react'
 import './App.css';
 import AthleteDashboard from './components/AthleteDashboard'
 import AdminDashboard from './components/AdminDashboard'
+import Login from './components/Login/Login'
 import TopBar from './components/TopBar'
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
 
 class App extends React.Component {
   render(){
     return <div >
       <TopBar/>
-      <AdminDashboard className="adminDashboard"/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path = '/' render={()=>
+                          (<AdminDashboard className="adminDashboard"/>)}/>
 
+          <Route exact path = '/login' render={() =>
+                          (<Login/>)}/>
+        </Switch>
+      </BrowserRouter>
       </div>
   }
 }
