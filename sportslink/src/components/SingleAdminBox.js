b5b9a8a7f8546ddb768e70ec016204772b337c7f
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-10-29 23:59:24 -0400

    horizontally aligned admin boxes

--- sportslink/src/components/SingleAdminBox.js
+++ sportslink/src/components/SingleAdminBox.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import SingleProfileBox from './SingleProfileBox'
 import '../components-css/SingleAdminBox.css'
 
 class SingleAdminBox extends React.Component{
     render(){
-        return <div>
-            <SingleProfileBox className="singleProfileBox"/>
+        return <div className="adminBox">
+            <div className="adminProfileBox">
+            <div className='profilePic'> <img src="logo512.png"/></div>
+
+            <div className='subBox'>
+            <p className="adminProfileName"> Username </p>
+            <p className="adminProfileDescription"> Description this is a very long long long long long long description</p>
+            </div>
+            </div>
+            <div className="adminActions">
+                <div className="adminButton">
+                    Suspend
+                </div>
+                <div className="adminButton">
+                    Remove
+                </div>
+
+                <div className="adminButton">
+                    Edit
+                </div>
+            </div>
         </div>
     }
 }
