df13c03fb25c6591da958bd71b5a722b2bb88880
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-11-02 16:44:03 -0500

    added react-uid

--- sportslink/src/components/Admin/AdminSearchResults.js
+++ sportslink/src/components/Admin/AdminSearchResults.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SingleAdminBox from './SingleAdminBox'
+import { uid } from "react-uid";
 
 class AdminSearchResults extends React.Component{
     constructor(props){
@@ -9,7 +10,7 @@ class AdminSearchResults extends React.Component{
     render(){
         return <div>
             {this.props.users.map((user) => {
-              return <SingleAdminBox user={user}/>
+              return <SingleAdminBox key={uid(user)} user={user}/>
             }
             )}
         </div>
--- sportslink/src/components/Athlete/SearchResults.js
+++ sportslink/src/components/Athlete/SearchResults.js
@@ -1,5 +1,7 @@
 import SingleProfileBox from './SingleProfileBox'
 import React from 'react'
+import { uid } from "react-uid";
+
 class SearchResults extends React.Component{
     constructor(props){
         super(props)
@@ -9,7 +11,7 @@ class SearchResults extends React.Component{
     render(){
         return <div>           
             {this.props.users.map((user) => {
-              return <SingleProfileBox user={user}/>
+              return <SingleProfileBox key={uid(user)} user={user}/>
             }
             )}
             </div>    
