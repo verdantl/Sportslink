f67ef10f0e60b65f6f3f61071b237d4d991bde08
ArjunPandher <arjunpandher@gmail.com>
2020-11-03 00:48:24 -0500

    Fixed a few issues with the text message layout

--- sportslink/src/components/Messaging/Messaging.js
+++ sportslink/src/components/Messaging/Messaging.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextList from './TextList';
-import "./Messaging.css"
+import Contacts from './Contacts';
+import "./Messaging.css";
 
 class Messaging extends React.Component{
 
@@ -27,18 +28,25 @@ class Messaging extends React.Component{
                     // TODO: Add search bar, list of contacts
                     // TODO: Finish layout for contacts
                 }
-                
+                <Contacts messages={this.state.messages} currUser={this.state.currUser}/>
             </div>
-            <div className="texts">
-                {
-                    // TODO: Autogenerate texts through backend
-                }
-                <TextList messages={this.state.messages} currUser={this.state.currUser}/>
-            </div>
-            <div className="sendMessageForm">
-                {
-                    // TODO: Finish layout for sendMessageForm
-                }
+            <div className="rightContent">
+                <div className="contactHeader">
+                    {
+                        //TODO: Finish layout for Contact Header
+                    }
+                </div>
+                <div className="texts">
+                    {
+                        // TODO: Autogenerate texts through backend
+                    }
+                    <TextList messages={this.state.messages} currUser={this.state.currUser}/>
+                </div>
+                <div className="sendMessageForm">
+                    {
+                        // TODO: Finish layout for sendMessageForm
+                    }
+                </div>
             </div>
         </div>
     }
