b26275ea8655f7692e26a6f1730ec7d27b6b2577
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-11-05 01:13:24 -0500

    added hover light on top bar

--- sportslink/src/components/TopBar.js
+++ sportslink/src/components/TopBar.js
@@ -4,11 +4,46 @@ import HomeIcon from '@material-ui/icons/Home';
 import './TopBar.css';
 class TopBar extends React.Component{
 
+    state = {
+        homeIcon: 'homeIcon',
+        settingsIcon: 'settingsIcon'
+    }
+
+    handleHover = (event) => {
+        console.log(event.target.parentElement)
+        if (event.target.parentElement.classList[0] === 'homeIcon'){
+            this.setState({homeIcon: 'homeIconHover'})
+
+        }
+        else if (event.target.parentElement.classList[0] === 'settingsIcon'){
+            this.setState({settingsIcon: 'settingsIconHover'})
+
+        }
+
+    }
+
+    handleHoverOff = (event) => {
+        if (event.target.parentElement.classList[0] === 'homeIconHover'){
+            this.setState({homeIcon: 'homeIcon'})
+
+        }
+        else if (event.target.parentElement.classList[0] === 'settingsIconHover'){
+            this.setState({settingsIcon: 'settingsIcon'})
+
+        }
+    }
+
+
     render(){
         return <div className="topBar">
-
-            <HomeIcon className="homeIcon"/>
-            <SettingsIcon className="settingsIcon"/>
+            <ul>
+            <li className={this.state.homeIcon}>
+            <HomeIcon className='home' onMouseEnter={this.handleHover} onMouseLeave={this.handleHoverOff}/>
+            </li>
+            <li className={this.state.settingsIcon}>
+            <SettingsIcon className='settings' onMouseEnter={this.handleHover} onMouseLeave={this.handleHoverOff}/>
+            </li>
+            </ul>
 
             </div>
     }
