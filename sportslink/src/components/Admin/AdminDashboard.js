02636f3ada03b8375fe4637536fd918d732cf22f
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-11-08 18:09:35 -0500

    added filters to admin dashboard

--- sportslink/src/components/Admin/AdminDashboard.js
+++ sportslink/src/components/Admin/AdminDashboard.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import './AdminDashboard.css'
-import SingleAdminBox from './SingleAdminBox'
 import AdminSearchResults from "./AdminSearchResults"
+import AdminSearchBox from './AdminSearchBox'
+import lebron from '../images/lebron.jpg'
+import harden from '../images/harden.jpg'
+import kawhi from '../images/kawhi.jpg'
+import durant from '../images/durant.jpg'
+import AdminSideFilters from './AdminSideFilters'
 
 class AdminDashboard extends React.Component{
     state = {
 
       users: [
-        {image: "", name: 'James Harden', description: "No. 13, Houston Rockets. 3x scoring champion"},
-        {image: "", name: "Kawhi Leonard  ", description: "2x Finals MVP, LA Clippers SF"},
-        {image: "", name: "Kevin Durant", description: "NBA Player for the Brooklyn Nets"}
+        {image: lebron, name: "Lebron James", description:'Point guard for the Los Angeles Lakers. 4 time NBA champion, 4x Finals MVP, 4x Regular Season MVP.'},
+        {image: harden, name: 'James Harden', description: "No. 13, Houston Rockets. 3x scoring champion"},
+        {image: kawhi, name: "Kawhi Leonard  ", description: "2x Finals MVP, LA Clippers SF"},
+        {image: durant, name: "Kevin Durant", description: "NBA Player for the Brooklyn Nets"}
       ]
     }
 
     render(){
-        return <div>
+        return <div className="adminDashboard">
+          <AdminSideFilters/>
+          <div className="adminRightColumn">
+          <AdminSearchBox/>
+
           <AdminSearchResults users={this.state.users}/>
+          </div>
+
 
         </div>
     }
