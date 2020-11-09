d76659d44b7e2e4f3098ab69248f586773f87574
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-11-09 01:22:51 -0500

    added posts to search results

--- sportslink/src/components/Athlete/SearchResults.js
+++ sportslink/src/components/Athlete/SearchResults.js
@@ -1,17 +1,27 @@
 import SingleProfileBox from './SingleProfileBox'
 import React from 'react'
+import Posts from '../Dashboard/Posts'
 import { uid } from "react-uid";
 
 class SearchResults extends React.Component{
 
 
     render(){
-        return <div>           
+        return <div>
+          <p className="searchResultTitle">Users</p>
+
+          <div className="searchUsers">  
+
             {this.props.users.map((user) => {
               return <SingleProfileBox key={uid(user)} user={user}/>
             }
             )}
-            </div>    
+          </div>   
+          <div className="searchPosts">
+          <p className="searchResultTitle">Posts</p>
+          <Posts posts={this.props.posts}/></div>     
+
+          </div>    
 
     }
 }
