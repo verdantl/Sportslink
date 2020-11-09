5a0a9227000cebb7b59f5714731e9585e6134a4a
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-11-08 22:32:00 -0500

    added return and export to posts and newpost

---
+++ sportslink/src/components/Dashboard/NewPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class NewPost extends React.Component{
+    render(){
+        return <div>
+            
+        </div>
+    }
+}
+
+export default NewPost
---
+++ sportslink/src/components/Dashboard/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './posts.css'
+
+class Posts extends React.Component{
+    render(){
+        return <div>
+
+        </div>
+    }
+}
+
+export default Posts
