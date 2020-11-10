e98c51b631ebc6ba75e60d408b90f00211362242
Abdul Arif <milkywayandbeyond@.gmail.com>
2020-11-09 21:15:51 -0500

    profile editing working

--- sportslink/src/components/Profile/Profile.js
+++ sportslink/src/components/Profile/Profile.js
@@ -23,23 +23,53 @@ class Profile extends React.Component{
             sports:"Basketball",
             contact: "",
             accomplishments: ["4x NBA Champion", '4x NBA Finals MVP', '4x NBA MVP', '16x NBA All Star', '13x All-NBA First Team'],
-            experience: [{title: 'Point Guard', organization: 'Los Angeles Lakers', league: "NBA", stats: {'2019': '25 ppg, 10apg'},
+            experience: [{id: '0', title: 'Point Guard', organization: 'Los Angeles Lakers', league: "NBA", stats: {'2019': '25 ppg, 10apg'},
             description: 'Led Lakers to a championship in the NBA Bubble 2020, averaging almost 30 ppg in the Finals against the Miami Heat',
             years: '2018-2020'},
-            {title: 'Small Forward', organization: 'Cleveland Cavaliers', league: "NBA", stats:{},
+            {id: '1', title: 'Small Forward', organization: 'Cleveland Cavaliers', league: "NBA", stats:{},
             description: 'Made the NBA Finals in all four years, came back from a 3-1 deficit and won a championship in the 2016 NBA FInals', years: '2014-2018'},
-            {title: 'Small Forward', organization: 'Miami Heat', league: "NBA", stats:{},
+            {id: '2', title: 'Small Forward', organization: 'Miami Heat', league: "NBA", stats:{},
             description: 'Made the NBA Finals in all four years, delivered two championships and won Finals MVP in 2012 and 2013', years: '2010-2014'},
-            {title: 'Small Forward', organization: 'Cleveland Cavaliers', league: "NBA", stats:{},
+            {id: '3', title: 'Small Forward', organization: 'Cleveland Cavaliers', league: "NBA", stats:{},
             description: 'Took the team to an NBA Finals in 2009, averaged over 25 ppg', years: '2003-2010'}]
             }
     }
 
+    handleButtonOff = (event) => {
+        this.setState({addButtonClass: 'addExperienceButton'})
+
+    }
+    updateExperience = (id, title, organization, league, stats, description, years) => {
+        console.log('this.state.user.experience');
+        console.dir(this.state.user.experience);
+        console.log('id', id);
+        console.log('league', league);
+        
+        var experiences = this.state.user.experience;
+        var i;
+        for (i in experiences){
+            console.dir(experiences[i])
+            if (experiences[i].id == id){
+                experiences[i].title = title;
+                experiences[i].organization = organization;
+                experiences[i].league = league;
+                experiences[i].stats = stats;
+                experiences[i].description = description;
+                experiences[i].years = years;
+            }
+        }
+        console.dir(experiences)
+        this.setState({experience: experiences});
+    }
+
     render(){
+        console.log('this profile')
+        console.dir(this)
+        //this.updateExperience('1', 'title', 'organization', 'league', 'stats', 'description', 'years')
         return <div className="profile">
             <div className="achievements">
                 <div></div>
-                <Experience experience={this.state.user.experience}/>
+                <Experience experience={this.state.user.experience} updateExperience={this.updateExperience.bind(this)}/>
                 <div className="profileRightColumn">
                 <Images images={this.state.user.images}/>
                 <Career accomplishments={this.state.user.accomplishments}/>
