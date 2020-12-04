b93342c1b0e1e5cd5b458b79963bd5de41f95039
ArjunPandher <arjunpandher@gmail.com>
2020-12-04 02:32:39 -0500

    Added schema for Message

---
+++ models/message.js
@@ -0,0 +1,47 @@
+'use strict';
+const mongoose = require('mongoose');
+/*
+    messages: [
+        {
+            messageID: 102,
+            userID: 'UserIDGoesHere2',
+            messageData: 'This is an incoming message'
+        },
+        {
+            messageID: 103,
+            userID: 'currUserID',
+            messageData: 'This is an outgoing message'
+        }
+    ]
+*/
+
+// Schema used for messages.
+const MessageSchema = new mongoose.Schema({
+    messageID: {
+        type: Number,
+        required: true,
+        minlength: 1
+    },
+    sentUsername: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    toUsername: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    messageData: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+})
+
+const Message = mongoose.model("message", MessageSchema)
+
+module.exports = { Message }
