0179b89944f75e18ae031904f6c492a5d39a51aa
ArjunPandher <arjunpandher@gmail.com>
2020-12-05 19:21:34 -0500

    Created and completed conversations action file

--- sportslink/src/actions/conversations.js
+++ sportslink/src/actions/conversations.js
@@ -1,3 +1,89 @@
-export function getMessages() {
-    
+export function getConversations(messaging) {
+    const url = "api/conversations"
+    fetch(url)
+        .then(res => {
+            if (res.status === 200) {
+                // return a promise that resolves with the JSON body
+                return res.json();
+            } else {
+                alert("Could not get conversations");
+            }
+        })
+        .then(json => {
+            messaging.setState({conversations: json})
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
+
+export function createNewConversation(currUser, otherUser, messaging) {
+    const url = "/api/conversation"
+
+    const request = new Request(url, {
+        method: "post",
+        body: {sentUsername: currUser, toUsername: otherUser},
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+    fetch(request)
+        .then(res => {
+            if (res.status === 200) {
+                // return a promise that resolves with the JSON body
+                messaging.setState({
+                    message: {
+                        body: "Success: Created a new conversation.",
+                        type: "success"
+                    }
+                });
+            } else {
+                dashboardComp.setState({
+                    message: {
+                        body: "Error: Could not create conversation.",
+                        type: "error"
+                    }
+                });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+export function createNewMessage(currUser, conversationID, messageData, messaging) {
+    const url = "api/conversation/" + conversationID + "/message"
+
+    const request = new Request(url, {
+        method: "post",
+        body: {id: conversationID, sentUsername: currUser, messageData: messageData},
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+    fetch(request)
+        .then(res => {
+            if (res.status === 200) {
+                // return a promise that resolves with the JSON body
+                messaging.setState({
+                    message: {
+                        body: "Success: Created a new message.",
+                        type: "success"
+                    }
+                });
+            } else {
+                dashboardComp.setState({
+                    message: {
+                        body: "Error: Could not create message.",
+                        type: "error"
+                    }
+                });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
