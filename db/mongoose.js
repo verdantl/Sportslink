06c0aaa8f68eea43f42867fdfa2b00c2555a4f19
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-12-07 23:46:05 -0500

    fixed mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 
 /* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.'mongodb://localhost:27017/SportsLinkAPI'
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://team44:sportslink@cluster0.erqyh.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/SportsLinkAPI'
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
