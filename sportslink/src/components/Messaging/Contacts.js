872bb4c8004d1fd055e19a56c05b8f7ede32f386
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-12-11 17:37:03 -0500

    changed contact to name instead of username

--- sportslink/src/components/Messaging/Contacts.js
+++ sportslink/src/components/Messaging/Contacts.js
@@ -18,7 +18,7 @@ class Contacts extends React.Component{
             <div key={"contact-" + uid(contact.userID)} id={"contact-" + contact.userID}>
                 <div className="contactItem">
                     <img className="contactImage" src={!this.state.users.filter(user => user.username === contact.userID)[0]? null : this.state.users.filter(user => user.username === contact.userID)[0].image} alt="Profile Pic"></img>
-                    <h6 className="contactName">{contact.userID}</h6>
+                    <h6 className="contactName">{!this.state.users.filter(user => user.username === contact.userID)[0]? null : this.state.users.filter(user => user.username === contact.userID)[0].name}</h6>
                     <p className="contactText">{contact.lastMessage}</p>
                     <hr/>
                 </div>
